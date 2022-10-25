f38bfaa1a43222c0d147aeb7c71acb7d817aca95
Osman Onat <mail@osmanonat.com.tr>
2022-10-25 23:56:54 +0300

    Add Check Role And Loader
    Add Check Role And Loader

---
+++ wa-widget-icon-oswp/plugin/loader.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Require Support Class
+ */
+require "support.php";
+
+/**
+ * Require Client
+ */
+require "client.php";
+
+/**
+ * Check Role Control For Admin
+ */
+roleControl( ["administrator"] , "admin.php" );
+
+
+
+?>
